80c35c5da99a10f17f6dbf17196e68a52033ea9d
Sonu Prajapati <sonukn089@gmail.com>
2017-09-06 00:20:11 +0530

    Blank

--- Chapter1/Module.java
+++
@@ -1,9 +0,0 @@
-class Module {
-
-	public static void main(String[] args) {
-		
-        
-
-		System.out.println(1 % 5);
-	}
-}
--- Chapter3/TestBranch.java
+++
@@ -1,5 +0,0 @@
-class TestBranch {
-	public static void main(String[] args) {
-		System.out.print('TestBranch');
-	}
-}
